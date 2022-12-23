40c9a0113a29600f858b9ac2a99b1fbe3cbea350
Sven "qoreQyaS" Grewe <git@gucke.net>
2022-12-23 05:40:12 +0100

    :bug: Fix many syntax errors
    Signed-off-by: Sven "qoreQyaS" Grewe <git@gucke.net>

--- sources/git-checkout.zsh
+++ sources/git-checkout.zsh
@@ -4,8 +4,7 @@ case $group in
     git diff $word | delta
     ;;
   'recent commit object name')
-    git log --color=always $word | perl -pe$(jq -j '.[] as $i | "s=" + $i.code + "=" + $i.emoji + "=g;"' ~/.gitmoji/gitmojis.json)
-    | delta
+    git log --color=always $word | perl -pe$(jq -j '.[] as $i | "s=" + $i.code + "=" + $i.emoji + "=g;"' ~/.gitmoji/gitmojis.json) | delta
     ;;
   *)
     git log --color=always $word | perl -pe$(jq -j '.[] as $i | "s=" + $i.code + "=" + $i.emoji + "=g;"' ~/.gitmoji/gitmojis.json)
