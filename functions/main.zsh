523a68d6221b85f8bfdb723de75d93ab0dd428e3
Wu Zhenyu <wuzhenyu@ustc.edu>
2023-05-25 00:25:05 +0800

    :bug: Fix #6, use ~/.lessfilter
    Fix missing $@ of functions
    Add nmcli

--- functions/main.zsh
+++ functions/main.zsh
@@ -1,11 +1,15 @@
 # define some functions to avoid any error that these programs don't exist
-(($+commands[bat])) || bat() {cat}
-(($+commands[mdcat])) || mdcat() {bat}
-(($+commands[pygmentize])) || pygmentize() {cat}
-(($+commands[delta])) || delta() {cat}
-(($+commands[less])) || less() {ls -l}
-(($+commands[finger])) || finger() {(($+commands[pinky])) && pinky $@ || whoami}
-(($+commands[pandoc])) || pandoc() {cat ${@[-1]}}
+(($+commands[bat])) || bat() {command cat $@}
+(($+commands[mdcat])) || mdcat() {command cat $@}
+(($+commands[pygmentize])) || pygmentize() {command cat $@}
+(($+commands[delta])) || delta() {command cat $@}
+# https://github.com/Freed-Wu/fzf-tab-source/issues/6
+(($+commands[less])) && [ -x ~/.lessfilter ] &&
+  less() {~/.lessfilter $@ || command less $@} || (($+commands[less])) ||
+    less() {command ls -l $@}
+(($+commands[finger])) || (($+commands[pinky])) &&
+  finger() {command pinky $@} || finger() {command whoami}
+(($+commands[pandoc])) || pandoc() {command cat ${@[-1]}}
 (($+commands[grc])) || grc() {eval ${@[2,-1]}}
 (($+commands[jq])) || jq() {echo ';'}
 
---
+++ sources/nmcli.zsh
@@ -0,0 +1,6 @@
+# :fzf-tab:complete:(\\|*/|)nmcli:argument-1
+case $group in
+command)
+  nmcli $word help
+  ;;
+esac
