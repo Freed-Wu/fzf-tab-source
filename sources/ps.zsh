aa2d17f4b540402d674c872610120bf9bdd5f8ba
Wu, Zhenyu <wuzhenyu@ustc.edu>
2024-04-15 23:40:48 +0800

    :bug: Fix #17, support darwin's ps

--- sources/ps.zsh
+++ sources/ps.zsh
@@ -1,3 +1,6 @@
 # :fzf-tab:complete:(\\|*/|)(kill|ps):argument-rest --preview-window=down:3:wrap
+if [[ $OSTYPE == darwin* ]]; then
+  alias -g -- --no-headers='| tail -n1'
+fi
 [[ $group == 'process ID' ]] &&
-  ps -p$word -wocmd --no-headers | bat -lsh
+  ps -p$word -wocommand --no-headers | bat -lsh
