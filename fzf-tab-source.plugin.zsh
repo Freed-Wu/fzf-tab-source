1ee4a320822b7b13c4761a07cb6b39c7bb678921
Wu, Zhenyu <wuzhenyu@ustc.edu>
2024-06-16 16:09:24 +0800

    :adhesive_bandage: Override $FZF_DEFAULT_OPTS's --preview-window

--- fzf-tab-source.plugin.zsh
+++ fzf-tab-source.plugin.zsh
@@ -27,8 +27,9 @@ function () {
     done < $src
     zstyle $ctx fzf-preview "src="\""$src"\"" . "\""$dir"\""/functions/main.zsh"
     flags=${arr[2]}
-    if [[ -n $flags ]]; then
-      zstyle $ctx fzf-flags $flags
-    fi
+    # https://github.com/Aloxaf/fzf-tab/issues/282
+    # https://github.com/junegunn/fzf/issues/2822
+    # $word = {2} make a wrong --preview-window=+{2}
+    zstyle $ctx fzf-flags ${flags:---preview-window=+0}
   done
 } $0
